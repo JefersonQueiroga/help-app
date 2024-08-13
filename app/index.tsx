979ba34ae577002e0748304e0ac735aae030975c
Jeferson Queiroga <jefersonqueiroga@gmail.com>
2024-08-13 14:47:07 -0300

    feat: add yup

--- app/index.tsx
+++ app/index.tsx
@@ -4,9 +4,28 @@ import { Input } from '@/components/Input';
 import { Button } from '@/components/Button';
 import { Envelope, Key } from 'phosphor-react-native';
 import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+
+//npm install react-hook-form yup @hookform/resolvers
+const schema = yup
+  .object({
+    email: yup.string().required('Email é obrigatório').email('Email inválido'),
+    password: yup
+      .string()
+      .required('Senha é obrigatória')
+      .min(6, 'A senha deve ter no mínimo 6 caracteres')
+      .matches(
+        /^(?=.*[!@#$%^&*(),.?":{}|<>])/,
+        'A senha deve conter pelo menos um caractere especial'
+      ),
+  })
+  .required();
 
 export default function HomeScreen() {
-  const { control, handleSubmit, formState: { errors } } = useForm();
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   function onSubmit ( data: any ) {
     Alert.alert('Sucesso', `Login realizado com sucesso.\nE-mail: ${data.email}\nSenha: ${data.password}`);
@@ -31,12 +50,13 @@ export default function HomeScreen() {
                 onChangeText={onChange}
                 onBlur={onBlur}
                 value={value}
+                error={!!errors.email} 
+                errorMessage={errors.email?.message as string}
+                isValid={!errors.email && !!value}
                 icon={<Envelope color="#7C7C8A" size={24} />}
               />
         )}
-      />  
-      {errors.email && <Text>{errors.email.message as string}</Text>}
-      
+      />        
 
         <Controller
           control={control}
@@ -48,12 +68,14 @@ export default function HomeScreen() {
               onChangeText={onChange}
               onBlur={onBlur}
               value={value}
+              error={!!errors.password}
+              errorMessage={errors.password?.message as string}
+              isValid={!errors.password && !!value}
               icon={<Key color="#7C7C8A" size={24} />}
             />
           )}
         />
-        {errors.password && <Text>{errors.password.message as string}</Text>}
-        
+                
         <Button title='Entrar' onPress={handleSubmit(onSubmit)} />
 
 
@@ -70,15 +92,16 @@ const styles = StyleSheet.create({
     padding: 20
   },
   reactLogo: {
-    marginTop: 50,
+    marginTop: 70,
     height: 90,
     width: 165
   },
   textTitle: {
-    fontSize: 20,
+    fontSize: 22,
     fontWeight: 'bold',
     color: '#E1E1E6',
-    marginTop: 80 
+    marginTop: 80, 
+    marginBottom: 24,
   }
 
 });
--- components/Input.tsx
+++ components/Input.tsx
@@ -5,34 +5,64 @@ import { ReactNode } from 'react';
 interface InputProp extends TextInputProps {
   placeholder: string;
   icon?: ReactNode;
+  error?: boolean;  
+  errorMessage?: string;
+  isValid?: boolean; 
 }
 
 
-export function Input({ placeholder, icon, ...rest}: InputProp){
+export function Input({ placeholder, error,errorMessage, isValid, icon, ...rest}: InputProp){
   return (
-    <View style={styles.container}>
-      {icon}
-      <TextInput placeholder={placeholder} {...rest}
-       placeholderTextColor="#7C7C8A"
-      style={ styles.textLabel}/>
+    <View style={styles.wrapper}>
+      <View style={[
+            styles.container,
+            error && styles.errorBorder, // Borda vermelha se houver erro
+            isValid && !error && styles.successBorder, // Borda verde se válido
+            ]}>
+        {icon}
+        <TextInput placeholder={placeholder} {...rest}
+        placeholderTextColor="#7C7C8A"
+        style={ styles.textLabel}/>
+      </View>
+      {error && errorMessage && <Text style={styles.errorText}>{errorMessage}</Text>}
     </View>  
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        paddingLeft: 16,
-        width: '100%',
-        backgroundColor: '#121214',
-        height: 56,
-        alignItems: 'center',
-        marginTop: 16,
-        borderRadius: 8,
-    },
-    textLabel: {
-        color: '#7C7C8A',
-        marginLeft: 8,
-        fontSize: 16,
-    } 
+  wrapper: {
+    width: '100%',
+    marginBottom: 16,
+  },
+  container: {
+    flexDirection: 'row',
+    paddingLeft: 16,
+    width: '100%',
+    backgroundColor: '#121214',
+    height: 56,
+    alignItems: 'center',
+    borderRadius: 8,
+    borderColor: '#7C7C8A',
+  },
+  textLabel: {
+    color: '#7C7C8A',
+    marginLeft: 8,
+    fontSize: 16,
+    flex: 1,
+  },
+  errorBorder: {
+    borderWidth: 1,
+    borderColor: '#FF4C4C',
+  },
+  errorText: {
+    color: '#FF4C4C',
+    marginTop: 4,
+    marginLeft: 16,
+    fontSize: 14,
+  },
+  successBorder: {
+    borderWidth: 1,
+    borderColor: '#00B37E', 
+  },  
+
 });
